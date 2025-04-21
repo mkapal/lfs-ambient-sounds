b36ce47de574981d3d72e4a75c86e9534ec017e3
Martin Kapal <flamecze@gmail.com>
2025-04-21 19:39:57 +0200

    Place holder public readme

---
+++ public/README.txt
@@ -0,0 +1,12 @@
+LFS Ambient Sounds
+==================
+
+Setup
+-----
+
+1) Open the main config file called "config.toml"
+
+Authors
+-------
+Martin Kapal (Flame CZE)
+https://github.com/mkapal/lfs-ambient-sounds
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,6 @@
+rm -rf dist/bin && \
 pkg -o "dist/bin/LFS Ambient Sounds.exe" . && \
-cp config.toml dist/bin/ && \
+cp config.toml public/README.txt dist/bin/ && \
 mkdir dist/bin/sounds && \
 cp sounds/global_street_ambience.mp3 sounds/SirenSlow.wav sounds/WindExternal.wav dist/bin/sounds/ && \
 mkdir dist/bin/tracks && \
